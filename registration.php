e892eca2b413c06966969ef6119620b65a4d48ab
tsuchiya <tsuchiya@veriteworks.co.jp>
2017-06-06 09:29:43 +0900

    add settings

---
+++ registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Copyright © 2016 Veriteworks. All rights reserved.
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Veriteworks_Helloimg',
+    __DIR__
+);
