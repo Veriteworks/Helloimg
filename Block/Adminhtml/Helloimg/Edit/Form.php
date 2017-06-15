02d20a3f7302de31ffb8f88193142dfaf70fdba1
tsuchiyawantan <tsuchiya.momoko@is.ocha.ac.jp>
2017-06-15 14:52:25 +0900

    add tab

--- Block/Adminhtml/Helloimg/Edit/Form.php
+++ Block/Adminhtml/Helloimg/Edit/Form.php
@@ -1,98 +1,21 @@
 <?php
-
 namespace Veriteworks\Helloimg\Block\Adminhtml\Helloimg\Edit;
 
-
 /**
- * Adminhtml Add New Row Form.
+ * Class Form
+ * @package Veriteworks\Helloimg\Block\Adminhtml\Slider\Edit
  */
 class Form extends \Magento\Backend\Block\Widget\Form\Generic
 {
     /**
-     * @var \Magento\Store\Model\System\Store
-     */
-    protected $_systemStore;
-
-    /**
-     * @param \Magento\Backend\Block\Template\Context $context
-     * @param \Magento\Framework\Registry             $registry
-     * @param \Magento\Framework\Data\FormFactory     $formFactory
-     * @param array                                   $data
-     */
-    public function __construct(
-        \Magento\Backend\Block\Template\Context $context,
-        \Magento\Framework\Registry $registry,
-        \Magento\Framework\Data\FormFactory $formFactory,
-        \Magento\Cms\Model\Wysiwyg\Config $wysiwygConfig,
-        array $data = []
-    )
-    {
-        $this->_wysiwygConfig = $wysiwygConfig;
-        parent::__construct($context, $registry, $formFactory, $data);
-    }
-
-    /**
-     * Prepare form.
-     *
      * @return $this
      */
     protected function _prepareForm()
     {
-        $dateFormat = $this->_localeDate->getDateFormat(\IntlDateFormatter::SHORT);
-        $model = $this->_coreRegistry->registry('row_data');
+        /** @var \Magento\Framework\Data\Form $form */
         $form = $this->_formFactory->create(
-            ['data' => [
-                'id' => 'edit_form',
-                'enctype' => 'multipart/form-data',
-                'action' => $this->getData('action'),
-                'method' => 'post'
-            ]
-            ]
+            ['data' => ['id' => 'edit_form', 'action' => $this->getData('action'), 'method' => 'post']]
         );
-
-        $form->setHtmlIdPrefix('helloimg_');
-        if ($model->getImgId()) {
-            $fieldset = $form->addFieldset(
-                'base_fieldset',
-                ['legend' => __('Edit Row Data'), 'class' => 'fieldset-wide']
-            );
-            $fieldset->addField('img_id', 'hidden', ['name' => 'img_id']);
-        } else {
-            $fieldset = $form->addFieldset(
-                'base_fieldset',
-                ['legend' => __('Add Row Data'), 'class' => 'fieldset-wide']
-            );
-        }
-
-       // $wysiwygConfig = $this->_wysiwygConfig->getConfig(['tab_id' => $this->getTabId()]);
-
-
-        $fieldset->addField(
-            'title',
-            'text',
-            [
-                'name' => 'title',
-                'label' => __('Title'),
-                'id' => 'title',
-                'title' => __('Title'),
-                'class' => 'required-entry',
-                'required' => true,
-            ]
-        );
-        $fieldset->addField(
-            'image',
-            'image',
-            [
-                'name' => 'image',
-                'label' => __('Image'),
-                'id' => 'image',
-                'title' => __('Image'),
-                'class' => 'required-entry',
-                'required' => true,
-            ]
-        );
-
-        $form->setValues($model->getData());
         $form->setUseContainer(true);
         $this->setForm($form);
 
---
+++ Block/Adminhtml/Helloimg/Edit/Tab/Form.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace Veriteworks\Helloimg\Block\Adminhtml\Helloimg\Edit\Tab;
+
+
+class Form extends \Magento\Backend\Block\Widget\Form\Generic implements \Magento\Backend\Block\Widget\Tab\TabInterface
+{
+    /**
+     * @var \Magento\Store\Model\System\Store
+     */
+    protected $_systemStore;
+
+    protected $_fieldFactory;
+
+    /**
+     * @param \Magento\Backend\Block\Template\Context $context
+     * @param \Magento\Framework\Registry             $registry
+     * @param \Magento\Framework\Data\FormFactory     $formFactory
+     * @param array                                   $data
+     */
+    public function __construct(
+        \Magento\Backend\Block\Template\Context $context,
+        \Magento\Framework\Registry $registry,
+        \Magento\Framework\Data\FormFactory $formFactory,
+        \Magento\Cms\Model\Wysiwyg\Config $wysiwygConfig,
+        \Magento\Config\Model\Config\Structure\Element\Dependency\FieldFactory $fieldFactory,
+        array $data = []
+    )
+    {
+        $this->_wysiwygConfig = $wysiwygConfig;
+        $this->_fieldFactory = $fieldFactory;
+        parent::__construct($context, $registry, $formFactory, $data);
+    }
+
+    /**
+     * Prepare form.
+     *
+     * @return $this
+     */
+    protected function _prepareForm()
+    {
+        $dateFormat = $this->_localeDate->getDateFormat(\IntlDateFormatter::SHORT);
+        $model = $this->_coreRegistry->registry('row_data');
+        $form = $this->_formFactory->create(
+            ['data' => [
+                'id' => 'edit_form',
+                'enctype' => 'multipart/form-data',
+                'action' => $this->getData('action'),
+                'method' => 'post'
+            ]
+            ]
+        );
+
+        $form->setHtmlIdPrefix('helloimg_');
+        if ($model->getImgId()) {
+            $fieldset = $form->addFieldset(
+                'base_fieldset',
+                ['legend' => __('Edit Row Data'), 'class' => 'fieldset-wide']
+            );
+            $fieldset->addField('img_id', 'hidden', ['name' => 'img_id']);
+        } else {
+            $fieldset = $form->addFieldset(
+                'base_fieldset',
+                ['legend' => __('Add Row Data'), 'class' => 'fieldset-wide']
+            );
+        }
+
+       // $wysiwygConfig = $this->_wysiwygConfig->getConfig(['tab_id' => $this->getTabId()]);
+
+
+        $fieldset->addField(
+            'title',
+            'text',
+            [
+                'name' => 'title',
+                'label' => __('Title'),
+                'id' => 'title',
+                'title' => __('Title'),
+                'class' => 'required-entry',
+                'required' => true,
+            ]
+        );
+        $fieldset->addField(
+            'image',
+            'image',
+            [
+                'name' => 'image',
+                'label' => __('Image'),
+                'id' => 'image',
+                'title' => __('Image'),
+                'class' => 'required-entry',
+                'required' => true,
+            ]
+        );
+
+        $form->setValues($model->getData());
+        $form->setUseContainer(true);
+        $this->setForm($form);
+
+        return parent::_prepareForm();
+    }
+    /**
+     * get dependency field.
+     *
+     * @return Magento\Config\Model\Config\Structure\Element\Dependency\Field [description]
+     */
+    public function getDependencyField($refField, $negative = false, $separator = ',', $fieldPrefix = '')
+    {
+        return $this->_fieldFactory->create(
+            ['fieldData' => ['value' => (string)$refField, 'negative' => $negative, 'separator' => $separator], 'fieldPrefix' => $fieldPrefix]
+        );
+    }
+
+    /**
+     * Prepare label for tab.
+     *
+     * @return string
+     */
+    public function getTabLabel()
+    {
+        return __('Level1');
+    }
+
+    /**
+     * Prepare title for tab.
+     *
+     * @return string
+     */
+    public function getTabTitle()
+    {
+        return __('Level1');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function canShowTab()
+    {
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isHidden()
+    {
+        return false;
+    }
+}
